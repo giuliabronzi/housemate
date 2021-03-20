b385bdc6888a6567c99ef892b4b394f8c062a246
Giulia Bronzi <giuliabronzi15@gmail.com>
2021-03-20 12:27:39 -0400

    flask added for search function, final touches for draft

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ import cs304dbi as dbi
 # import cs304dbi_sqlite3 as dbi
 
 import random
-#import details 
 import modules
-
+conn = dbi.connect()
 
 app.secret_key = 'your secret here'
 # replace that with a random key
@@ -24,22 +23,42 @@ app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
 # This gets us better error messages for certain common request errors
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 
+
+# route for home page
 @app.route('/')
 def index():
-    return render_template('main.html',title='Hello')
+    states = modules.listStatesWithListings(conn)
+    return render_template('main.html', states = states)
 
-@app.route('/listing/<int:hId>', methods=["GET", "POST"])
-def listing():
-    if request.method == 'GET':
-        return render_template('listing.html', title='Customized Greeting')
-    else:
-        return redirect
+# route for listing page specified by house ID
+@app.route('/listing/<int:hId>')
+def listing(hId):
+    house = modules.get_details(conn, hId)
+    return render_template('listing.html', 
+        address = house['address'], listingTitle = house['listingTitle'], 
+        username = house['username'], price = house['price'], 
+        city = house['city'], state = house['state'], 
+        bedroomNum  = house['bedroomNum'], roommatesNum = house['roommatesNum'],
+        bathroomNum = house['bathroomNum'], sqrft = house['sqrft'], 
+        area = house['area'], nearbySchools = house['nearbySchools'],
+        openDate = house['openDate'], closeDate = house['closeDate'], 
+        description = house['description'], availability = house['availability'])
 
+
+# route for submiting a listing
 @app.route('/submitListing/', methods=['GET','POST'])
 def submitListing():
     conn = dbi.connect()
+    #renders form if get
     if request.method == 'GET':
-        return render_template('submitListing.html')
+        stateCodes = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+                  "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+                  "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+                  "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+                  "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+        return render_template('submitListing.html', stateCodes = stateCodes)
+    #otherwise adds inserted data into listing table
     else: 
         address = request.form.get('address')
         listingTitle = request.form.get('listingTitle')
@@ -61,23 +80,29 @@ def submitListing():
         modules.insertListing(conn, address, listingTitle, username,
                              price, city, state, bedroomNum, roommatesNum, bathroomNum, sqrft, 
                              area, nearbySchools, openDate, closeDate, description, availability)
+        flash("Listing successfully submitted!")
         return render_template('submitListing.html')
 
-        #not sure if this is exactly right but something like this
-        # return redirect(url_for('listing', hId = select_inserted_id()))
 
+# route for searching for a listing
+# currently only using the details for state, city, and number of bedrooms
 @app.route('/search/', methods=['GET'])
 def search():
-    conn = dbi.connect()
-    curs = dbi.dict_cursor(conn)
     state = request.args.get('state')
     city = request.args.get('city')
     bedroomNum = request.args.get('bedroomNum')
-    matches = searchListings(conn, city, state, bedroomNum)
+    matches = modules.searchListings(conn, city, state, bedroomNum)
+
+    #goes straight to listing page if only one matching result
     if len(matches) == 1:     
-        return render_template('listing.html', variables...)
-    else:
+        return redirect(url_for('listing', hId = matches[0]['hId']))
+    #goes to list if more than one result
+    elif len(matches) > 1:
         return render_template('listingResults.html', listings = matches)
+    else:
+        flash("No listings fit your search criteria")
+        states = modules.listStatesWithListings(conn)
+        return render_template('main.html', states = states)
 
 @app.before_first_request
 def init_db():
--- modules.py
+++ modules.py
@@ -1,6 +1,14 @@
+# modules for app.py
+
 import cs304dbi as dbi
 
 
+def get_details(conn, hId):
+    '''Returns the lisiting details as a list of dictionaries.'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select * from listing where hId = %s;''',[hId])
+    return curs.fetchone()
+    
 
 def insertListing(conn, address, listingTitle, username,
                              price, city, state, bedroomNum, roommatesNum, bathroomNum, sqrft, 
@@ -18,7 +26,16 @@ def insertListing(conn, address, listingTitle, username,
 
 def searchListings(conn, city, state, bedroomNum):
         """Retrieves listings that match searched criteria"""
-        curs.execute('''select city, state, bedroomNum, description from 
-        listing where city = %s and state = %s and bedroomNum = %s''',
+        curs = dbi.dict_cursor(conn)
+        curs.execute('''select hId, city, state, bedroomNum, description from 
+        listing where city like %s and state = %s and bedroomNum = %s''',
         [city, state, bedroomNum])
         return curs.fetchall()
+
+
+def listStatesWithListings(conn):
+    """ Returns the state's of the available listing for
+        the drop down menu in the search form"""
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select distinct state from listing ''')
+    return curs.fetchall()
